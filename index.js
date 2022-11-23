e666a9d68e1f0f7e7a8eab47488133d8ad9162ea
Patrick <70089310+Patfarmurs@users.noreply.github.com>
2022-11-23 06:30:53 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,80 @@
+const menuBtn = document.querySelector('.menu-btn');
+const headerNavMobile = document.querySelector('.header-nav-mobile');
+const btnCloseNav = document.querySelector('.btn-close-nav');
+const linkPortfolio = document.querySelector('.link-portfolio');
+const linkAbout = document.querySelector('.link-about');
+const linkContact = document.querySelector('.link-contact');
+const arrayBtnPopup = document.querySelectorAll('.btn-popup');
+const form = document.querySelector('.form-input');
+const [fullName, firstName, lastName, email, textArea] = form.elements;
+const mediaqueryList = window.matchMedia('(max-width: 768px)');
+
+const screenTest = (e) => {
+  if (e.matches) {
+    fullName.required = true;
+    firstName.required = false;
+    lastName.required = false;
+  } else {
+    fullName.removeAttribute('required');
+    firstName.required = true;
+    lastName.required = true;
+  }
+};
+
+screenTest(mediaqueryList);
+mediaqueryList.addListener(screenTest);
+
+let inputData = {};
+if (localStorage.savedForm) {
+  inputData = JSON.parse(localStorage.getItem('savedForm'));
+}
+fullName.addEventListener('change', () => {
+  inputData.fullName = fullName.value;
+});
+
+firstName.addEventListener('change', () => {
+  inputData.firstName = firstName.value;
+});
+
+lastName.addEventListener('change', () => {
+  inputData.lastName = lastName.value;
+});
+
+email.addEventListener('change', () => {
+  inputData.email = email.value;
+});
+
+textArea.addEventListener('change', () => {
+  inputData.textArea = textArea.value;
+});
+
+const fillDataInput = () => {
+  if (inputData.fullName) {
+    fullName.value = inputData.fullName;
+  }
+  if (inputData.firstName) {
+    firstName.value = inputData.firstName;
+  }
+  if (inputData.lastName) {
+    lastName.value = inputData.lastName;
+  }
+  if (inputData.email) {
+    email.value = inputData.email;
+  }
+  if (inputData.textArea) {
+    textArea.value = inputData.textArea;
+  }
+};
+
+const populateFields = () => {
+  localStorage.setItem('savedForm', JSON.stringify(inputData));
+  fillDataInput();
+};
+
+populateFields();
+form.onchange = populateFields;
+const errMsgEmail = document.querySelector('small');
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.add('hidden');
+  headerNavMobile.classList.remove('hidden');
+});
