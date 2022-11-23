c272e096e063376961dfc3aea7370a938d09ef90
ilyass atlassi <atlassiilyass@gmail.com>
2022-11-23 16:16:47 +0100

    Add script for menu

--- index.js
+++ index.js
@@ -78,3 +78,23 @@ menuBtn.addEventListener('click', () => {
   menuBtn.classList.add('hidden');
   headerNavMobile.classList.remove('hidden');
 });
+
+btnCloseNav.addEventListener('click', () => {
+  menuBtn.classList.remove('hidden');
+  headerNavMobile.classList.add('hidden');
+});
+
+linkPortfolio.addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+  menuBtn.classList.remove('hidden');
+});
+
+linkAbout.addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+  menuBtn.classList.remove('hidden');
+});
+
+linkContact.addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+  menuBtn.classList.remove('hidden');
+});
