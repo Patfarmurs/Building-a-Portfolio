c644510bcbacc1a59a23a2e8324d586fefc3bf85
unknown <patrickkeita80@yahoo.com>
2023-09-30 11:16:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -109,12 +109,13 @@ form.addEventListener('submit', (e) => {
   }
 });
 
-/* const dataCards = [
+const dataCards = [
   {
     id: 1,
     url: './img/Math-magician.png',
     title: 'Math Magician',
-    description: `A calculator application created with React.`,
+    description: `A calculator application created with React. Users can perform basic operations 
+    such as addition, subtraction, multiplication, and division, as well as more advanced operations like modulo. `,
     tecnologies: {
       tech1: 'React',
       tech2: 'css',
@@ -128,7 +129,11 @@ form.addEventListener('submit', (e) => {
     id: 2,
     url: './img/expense-tracker-1.png',
     title: 'Budget App',
-    description: `Budget App: is a web-based rails application.`
+    description: `Budget App: is a web-based rails application, designed to help users manage their finances and track their expenses. 
+    It provides features such as user signup and login functionality to ensure secure access to personal budget information.
+    The application allows users to create and manage categories. 
+    Categories represent different types of expenses.`,
+
     tecnologies: {
       tech1: 'Rubi on rails',
       tech2: 'css',
@@ -140,62 +145,100 @@ form.addEventListener('submit', (e) => {
   },
 
   {
-    id: 3,
-    url: './img/Space Traveler-hub.png',
-    title: 'Budget App',
-    description: `Budget App: is a web-based rails application.`,
-    tecnologies: {
-      tech1: 'Rubi on rails',
-      tech2: 'css',
-      tech3: 'JavaScript',
-      tech4: 'html',
-    },
-    linkVersion: 'https://space-hub-traveller.netlify.app/',
-    linkSource: 'https://github.com/Patfarmurs/react-group-project',
-  },
+    id: 2,
+    url: './img/Space-Traveler-hub.png',
+    title: 'Space Traveler Hub',
+    description: `Space Traveler Hub: is a collaborative website that highlights the use of react/redux. 
+    A user can join the mission, reserve a Rocket, and cancel the reservation.`,
 
-  {
-    id: 4,
-    url: './img/Space Traveler-hub.png',
-    title: 'Space-Hub Traveller',
-    description: `A collaborative website that highlights the use of react/redux.`,
     tecnologies: {
       tech1: 'React',
       tech2: 'css',
       tech3: 'JavaScript',
       tech4: 'html',
     },
-    linkVersion: '',
+    linkVersion: 'space-hub-traveller.netlify.app',
     linkSource: 'https://github.com/Patfarmurs/react-group-project',
   },
+
+  {
+    id: 4,
+    url: './img/Book-Store1.png',
+    title: 'React Book Store',
+    description: `React Book Store: is a comprehensive web platform designed to facilitate book browsing and selection. 
+    It allows visitors to explore an extensive collection of books, manage their personal reading lists, and add their preferred books. `,
+
+    tecnologies: {
+      tech1: 'React',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linkVersion: 'https://react-book-store-7nwr.onrender.com/',
+    linkSource: 'https://github.com/Patfarmurs/react-book-store',
+  },
+
+  {
+    id: 5,
+    url: './img/Movie-List.png',
+    title: 'Movie List',
+    description: `Movie List: is a Microverce group project. It's an online entertainment streaming source for TV shows, and movies.
+     All data is preserved thanks to the external Tv Maze API service. In this App a user can add comments and likes.`,
+
+    tecnologies: {
+      tech1: 'Webpack',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linkVersion: 'https://bill7pearl.github.io/JS_Capstone/',
+    linkSource: 'https://github.com/Patfarmurs/JS_Capstone',
+  },
+
+  {
+    id: 6,
+    url: './img/Onwart.png',
+    title: 'Metrick WebApp',
+    description: `metrick-webapp: is a mobile web application that allows the user to create a list of metrics by utilizing React and Redux. 
+    The WebApp consists of two pages: the first page will display a list of items that can be filtered using the name, serving as the homepage. The second page will be for displaying the details of a particular item. `,
+
+    tecnologies: {
+      tech1: 'React',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linkVersion: 'https://bejewelled-elf-f1e263.netlify.app',
+    linkSource: 'https://github.com/Patfarmurs/metrics-webapp',
+  },
 ];
-*/
+
 const cardWorkDetailsMobile = (index) => `
   <article class="card-work-details-mobile">
           <button type="button" class="btn-close-details">
               <img src="./img/X-icon.svg" alt="X icon">
           </button>
           <div class="card-image-details-mobile">
-              <img src=${[index].url} alt="Snapshoot Portfolio">
+              <img src=${dataCards[index].url} alt="Snapshoot Portfolio">
           </div>
           <h2 class="card-title-mobile">
-              ${[index].title}
+              ${dataCards[index].title}
           </h2>
           <ul class="card-tags-mobile">
-              <li class="tag-mobile">${[index].tecnologies.tech1}</li>
-              <li class="tag-mobile">${[index].tecnologies.tech2}</li>
-              <li class="tag-mobile">${[index].tecnologies.tech3}</li>
-              <li class="tag-mobile">${[index].tecnologies.tech4}</li>
+              <li class="tag-mobile">${dataCards[index].tecnologies.tech1}</li>
+              <li class="tag-mobile">${dataCards[index].tecnologies.tech2}</li>
+              <li class="tag-mobile">${dataCards[index].tecnologies.tech3}</li>
+              <li class="tag-mobile">${dataCards[index].tecnologies.tech4}</li>
           </ul>
           <p class="card-paragraph-mobile">
-            ${[index].description}
+            ${dataCards[index].description}
           </p>
           <div class="card-button-mobile">
-                  <a href=${[index].linkVersion} class="btn-card-details-mobile">
+                  <a href=${dataCards[index].linkVersion} class="btn-card-details-mobile" target= "_blank" >
                   See Live
                   <img src="./img/see_live_icon.svg" alt="live icon">
                   </a>
-                  <a href=${[index].linkSource} class="btn-card-details-mobile">
+                  <a href=${dataCards[index].linkSource} class="btn-card-details-mobile" target= "_blank">
                   See Sources
                   <img src="./img/github.svg" alt="github icon">
                   </a>
